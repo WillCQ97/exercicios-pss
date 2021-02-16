c298c55608cd5fe3ae2a543158ba0273c14d17be
WillCQ97 <willian.cqueiroz@gmail.com>
2021-02-16 20:55:18 -0300

    Exercicio 01 enunciado

---
+++ Exercicio-01/src/main/java/br/ufes/willcq/App.java
@@ -0,0 +1,13 @@
+package br.ufes.willcq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Exercicio-01/src/main/java/br/ufes/willcq/Pedido.java
@@ -0,0 +1,36 @@
+package br.ufes.willcq;
+
+public class Pedido {
+
+    private enum Status {
+        PROCESSANDO, CANCELADO, ENVIADO
+    };
+
+    private Status status;
+    private String msg;
+
+    public void alterar() {
+        switch (status) {
+            case PROCESSANDO:
+                // código para alteração do pedido
+                break;
+
+            case CANCELADO:
+                msg = "Não é possível alterar um pedido cancelado";
+                throw new IllegalStateException(msg);
+
+            case ENVIADO:
+                msg = "Não é possível alterar um pedido enviado";
+                throw new IllegalStateException(msg);
+        }
+    }
+
+    public void cancelar() {
+
+    }
+
+    public void enviar() {
+
+    }
+
+}
