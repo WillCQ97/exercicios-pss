3edbd54abb1adab759650875a712c11e2ab8cc36
WillCQ97 <willian.cqueiroz@gmail.com>
2021-02-16 21:13:16 -0300

    Ex01. Implementaçao completa

--- Exercicio-01/src/main/java/br/ufes/willcq/Pedido.java
+++ Exercicio-01/src/main/java/br/ufes/willcq/Pedido.java
@@ -26,11 +26,38 @@ public class Pedido {
     }
 
     public void cancelar() {
+        switch (status) {
+            case PROCESSANDO:
+                status = Status.CANCELADO;
+                msg = "O pedido foi cancelado";
+                break;
 
+            case CANCELADO:
+                msg = "Não é possível alterar um pedido cancelado";
+                throw new IllegalStateException(msg);
+
+            case ENVIADO:
+                msg = "Não é possível alterar um pedido enviado";
+                throw new IllegalStateException(msg);
+        }
     }
 
     public void enviar() {
+        switch (status) {
+            case PROCESSANDO:
+                status = Status.ENVIADO;
+                msg = "O pedido foi enviado";
+                break;
 
+            case CANCELADO:
+                msg = "Não é possível alterar um pedido cancelado";
+                throw new IllegalStateException(msg);
+
+            case ENVIADO:
+                msg = "Não é possível alterar um pedido enviado";
+                throw new IllegalStateException(msg);
+        }
+    }
     }
 
 }
